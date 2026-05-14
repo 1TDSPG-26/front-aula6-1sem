7d698b294455c15ede7c728a3d5faeadca27f70e
GustavoNeriAndrade <gustavoneriandradee@gmail.com>
2026-05-14 09:00:36 -0300

    feat: Capturando elemento com querySelector, apresenta dados, TryCatch, cria objetos e realizando validação para realizar redirect com location.href

--- js/script.js
+++ js/script.js
@@ -1,44 +1,58 @@
 
+//Objeto INTERNO
+const usuarioDb = {
+    email:"email@email.com",
+    senha:"12345",
+    nome: "José Antonio",
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+}
+
+
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-console.log(botaoEntrar);
-
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
+botaoEntrar.addEventListener("click", function(e){
+    
+    e.preventDefault();
 
-botaoEntrar.addEventListener("click", function(){
-
-
-
-
-    //     //Para casa:
-//     //Capturar os campos de email e senha.
-//     //Armazenar em variáveis.
-//     //Imprimir seus valores.
-
-    const email = document.getElementById("idEmail");
-    const senha = document.getElementById("idSenha");
-
-    //Armazenando os dados em um Objeto:
-    //Declarando um Objeto
-    const usuario = {
-        email : email.value,
-        senha : senha.value
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+    const email = document.querySelector("#idEmail");
+    const senha = document.querySelector("#idSenha");
+    
+    //Armazenar os dados que chegam do form em um objeto:
+    const userForm = {
+        email: email.value,
+        senha: senha.value
     }
 
-    
-    //Imprimindo os dados dos campos:
+    //Validação
+    try {
 
-    //Concatenação padrão:
-    console.log("Email : " + email.value + " e senha: " + senha.value);
-    
-    //Impressão dos dados com Template Literal e interpolação:
-    console.log(`Email :  ${email.value}  e senha: ${senha.value}.`);
+        if (usuarioDb) {
 
-    //Para casa.
-    //Realizar os exercício da apostila até a página 64.
-    //Apresente os dados armazenados no Objeto usuário utilizando Template Literal:
+            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
+                alert("Login realizado com sucesso!")
+                window.location.href = "../index.html";
+            }else{
+                throw Error("Email ou senha incorretos")
+            }
+            
+        }else{
+            throw Error("Ocorreu um erro no acesso as informações do usuário.")
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+
+
+    
 
 });
