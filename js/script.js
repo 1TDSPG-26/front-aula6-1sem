2eb2a8c9ff53e065e375e7e023e09bf84bd0be50
TailyniDev <tailyni80@gmail.com>
2026-05-14 09:54:47 -0300

    feat : Janela modal com redirect e counter regressivo. Utilizando setInterval

--- js/script.js
+++ js/script.js
@@ -1,33 +1,97 @@
-// alert("olá mundo!");
-// var nome = prompt("Olá digite seu nome");
 
-// alert("Meu nome é " + nome);
-// console.log("Meu nome é " + nome);
-
-// HOISTING
-// var nome = "Alê";
-// let sobreNome = "Carlos";
-
-// if(true){
-
-//     var nome = "Joaquim";
-//     let sobreNome = "Das Couves";
-
-// }
-
-// console.log(nome);
-// console.log(sobreNome);
+//Objeto INTERNO
+const usuarioDb = {
+    email:"email@email.com",
+    senha:"12345",
+    nome: "José Antonio",
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+}
 
 
+//Capturando o botão de entrar
+const botaoEntrar = document.getElementById("btnEntrar");
 
-//Declarando variáveis em JS com let
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
-let nome   = "José";
-let idade  = 33;
-let status = false;
+botaoEntrar.addEventListener("click", function(e){
+    
+    e.preventDefault();
 
-//Imprimir os dados do nosso usuário JOSÉ com console.log usando concatenação simples:
-console.log(nome + " de " + idade +" anos está " + (status === true ? "empregado" : "desempregado") + " atualmente.");
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+    const email = document.querySelector("#idEmail");
+    const senha = document.querySelector("#idSenha");
+    
+    //Armazenar os dados que chegam do form em um objeto:
+    const userForm = {
+        email: email.value,
+        senha: senha.value
+    }
 
-//Exercício: agora escreva a mesma frase com template-literal, onde o texto e as variáveis ficam dentro de ` ` e as variáveis são apresentadas entre os símbolos de ${variável}.
-console.log( `${nome} de ${idade} anos está ${(status === true ? "empregado" : "desempregado")} atualmente.` );
+    //Validação
+    try {
+
+        if (usuarioDb) {
+
+            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
+                //Capturar o dialog
+                const modalDialog = document.querySelector("#meuModal");
+
+                //Abrindo o dialog e deixando o fundo livre.
+                // modalDialog.show();
+
+                //Abrindo o dialog e travando o fundo.
+                modalDialog.showModal();
+
+                
+                //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
+                const botaoModal = document.querySelector("#btnFechar");
+                botaoModal.addEventListener("click", function(){
+                    modalDialog.close();
+                });
+                
+                //Adicionar a mensagem de sucesso para o usuário no modal!
+                //Vamos usar o innerHTML para injetar 2 <p> com o texto na div=msg.
+                //Mas para isso necessitamos capturar a div=msg antes
+                const divMsg = document.querySelector("#msg");
+                
+                let contador = 5;
+
+                //Injetando os 2 <p> com a mensagem de sucesso.
+                divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+
+                //Temporizador de redirecionamento com setInterval.
+                const intervalo = setInterval( function(){
+                    
+                    contador--;
+                    divMsg.innerHTML = `<p>Login realizado com Sucesso!</p><p>Você será redirecionado em ${contador} segundos...</p>`;
+                    
+                    if(contador === 0){
+                        clearInterval(intervalo);
+                        //Redirecionamento do usuário para uma nova página!!
+                        window.location.href = "../index.html";
+                    }
+
+                }, 1000 );
+
+            }else{
+                throw Error("Email ou Senha incorretos.");
+            }
+            
+        }else{
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+
+});
+
+
+//Pra casa
+//Reutilzxar o MODAL para as mensagens de erro!!!
