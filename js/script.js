831a394a7cc38d29acac4d1f90279d3665225c41
arthurarrudaa <arthur.arruda.dev@gmail.com>
2026-05-14 08:25:31 -0300

    feat : refatorando o codigo por completo apos o css

--- js/script.js
+++ js/script.js
@@ -3,42 +3,17 @@
 const botaoEntrar = document.getElementById("btnEntrar");
 console.log(botaoEntrar);
 
-
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
-
 botaoEntrar.addEventListener("click", function(){
+    console.log("Botão de entrar clicado");
 
-
-
-
-    //     //Para casa:
-//     //Capturar os campos de email e senha.
-//     //Armazenar em variáveis.
-//     //Imprimir seus valores.
-
-    const email = document.getElementById("idEmail");
-    const senha = document.getElementById("idSenha");
-
-    //Armazenando os dados em um Objeto:
-    //Declarando um Objeto
-    const usuario = {
-        email : email.value,
-        senha : senha.value
-    }
-
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+    const email = document.querySelector("label[for='idEmail'] input")
     
-    //Imprimindo os dados dos campos:
-
-    //Concatenação padrão:
-    console.log("Email : " + email.value + " e senha: " + senha.value);
-    
-    //Impressão dos dados com Template Literal e interpolação:
-    console.log(`Email :  ${email.value}  e senha: ${senha.value}.`);
-
-    //Para casa.
-    //Realizar os exercício da apostila até a página 64.
-    //Apresente os dados armazenados no Objeto usuário utilizando Template Literal:
 
 });
