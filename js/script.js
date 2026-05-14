58eaad71e8b2d4942bb8e5a76a25a1b47518918b
Rikk <tarikmoussa90@gmail.com>
2026-05-14 09:52:22 -0300

    feat: main+modal.css and sJS refat

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ const usuarioDb = {
 
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-console.log(botaoEntrar);
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
@@ -38,7 +37,18 @@ botaoEntrar.addEventListener("click", function(e){
         if (usuarioDb) {
 
             if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-                alert("Login realizado com sucesso!");
+                //Capturar o dialog
+                const modalDialog = document.querySelector("#meuModal");
+
+                //Abrindo o dialog e deixando o fundo livre.
+                // modalDialog.show();
+
+                //Abrindo o dialog e travando o fundo.
+                modalDialog.showModal();
+                
+
+                //Redirecionamento do usuário para uma nova página!!
+                //window.location.href = "../index.html";
             }else{
                 throw Error("Email ou Senha incorretos.");
             }
@@ -51,8 +61,5 @@ botaoEntrar.addEventListener("click", function(e){
         console.error(error);
         alert(error);
     }
-
-
-    
 
 });
