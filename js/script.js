38821d5e032fb9406c8798364c426ffd23af2d6a
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-14 09:00:18 -0300

    feat: Capturando elementos com querySelector, apresenta dados, tryCatch, cria objetos e realiando validação para realizar redirect com location.href
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- js/script.js
+++ js/script.js
@@ -1,25 +1,56 @@
 
+//Objeto INTERNO
+const usuarioDb = {
+    email:"email@email.com",
+    senha:"12345",
+    nome: "José Antonio",
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+}
+
+
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-console.log(botaoEntrar);
-
-//Declarando variáveis em JS com let
-
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
-
-botaoEntrar.addEventListener("click", function(){
-    console.log("Botão de entrar clicado");
-
+botaoEntrar.addEventListener("click", function(e){
+    
+    e.preventDefault();
 
     //Para casa:
     //Capturar os campos de email e senha.
     //Armazenar em variáveis.
     //Imprimir seus valores.
+    const email = document.querySelector("#idEmail");
+    const senha = document.querySelector("#idSenha");
+    
+    //Armazenar os dados que chegam do form em um objeto:
+    const userForm = {
+        email: email.value,
+        senha: senha.value
+    }
 
+    //Validação
+    try {
 
+        if (usuarioDb) {
 
+            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
+                alert("Login realizado com sucesso!");
+                //Redirecionamento do usuário para uma nova página!!
+                window.location.href = "../index.html";
+            }else{
+                throw Error("Email ou Senha incorretos.");
+            }
+            
+        }else{
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
 
 });
