c267407a7731a00f64b957ab8703fe4e04595c05
RafaelSantos56 <rafarbcosta17@gmail.com>
2026-05-14 09:33:56 -0300

    feat: Estruturando o modalDialog com CSS e JS, adicionando o botão de controle para fechar.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,15 @@
 
 //Objeto INTERNO
 const usuarioDb = {
-    email: "email@email.com",
-    senha: "12345",
+    email:"email@email.com",
+    senha:"12345",
     nome: "José Antonio",
     avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
-
 }
 
 
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-console.log(botaoEntrar);
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
@@ -20,16 +18,13 @@ botaoEntrar.addEventListener("click", function(e){
     
     e.preventDefault();
 
-
     //Para casa:
     //Capturar os campos de email e senha.
     //Armazenar em variáveis.
     //Imprimir seus valores.
     const email = document.querySelector("#idEmail");
     const senha = document.querySelector("#idSenha");
-    console.log(email.value);
-    console.log(senha.value); 
-
+    
     //Armazenar os dados que chegam do form em um objeto:
     const userForm = {
         email: email.value,
@@ -39,25 +34,38 @@ botaoEntrar.addEventListener("click", function(e){
     //Validação
     try {
 
-        if(usuarioDb){
+        if (usuarioDb) {
 
             if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-                alert("Login realizado com sucesso!");
-                //Redirecionamento do usuário para uma nova página!
-                window.location.href = "../index.html";
-            } else{
+                //Capturar o dialog
+                const modalDialog = document.querySelector("#meuModal");
+
+                //Abrindo o dialog e deixando o fundo livre.
+                // modalDialog.show();
+
+                //Abrindo o dialog e travando o fundo.
+                modalDialog.showModal();
+
+                //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
+                const botaoModal = document.querySelector("#btnFechar");
+                botaoModal.addEventListener("click", function(){
+                    modalDialog.close();
+                });
+                
+
+                //Redirecionamento do usuário para uma nova página!!
+                //window.location.href = "../index.html";
+            }else{
                 throw Error("Email ou Senha incorretos.");
             }
-
+            
         }else{
-            throw Error("Ocorreu um erro no acesso as informações de usuário.")
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
         }
 
     } catch (error) {
         console.error(error);
         alert(error);
     }
-
-
 
 });
