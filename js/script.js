518c77d7ce51d9d5a5aa0e55bcdef08177e6e15e
alecarlosjesus <profalexandre.jesus@email.com.br>
2026-05-14 09:32:16 -0300

    feat: Estruturando o modalDialog com CSS e JS, adicionando o botão de controle para fechar.
    Signed-off-by: alecarlosjesus <profalexandre.jesus@email.com.br>

--- js/script.js
+++ js/script.js
@@ -37,9 +37,24 @@ botaoEntrar.addEventListener("click", function(e){
         if (usuarioDb) {
 
             if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-                alert("Login realizado com sucesso!");
+                //Capturar o dialog
+                const modalDialog = document.querySelector("#meuModal");
+
+                //Abrindo o dialog e deixando o fundo livre.
+                // modalDialog.show();
+
+                //Abrindo o dialog e travando o fundo.
+                modalDialog.showModal();
+
+                //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
+                const botaoModal = document.querySelector("#btnFechar");
+                botaoModal.addEventListener("click", function(){
+                    modalDialog.close();
+                });
+                
+
                 //Redirecionamento do usuário para uma nova página!!
-                window.location.href = "../index.html";
+                //window.location.href = "../index.html";
             }else{
                 throw Error("Email ou Senha incorretos.");
             }
