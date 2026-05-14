78366b979deb1944163523b2d1c2756cc409468d
manuelalramos <manuela.lima.r@outlook.com>
2026-05-14 09:08:17 -0300

    feat : Capturando elementos com querySelector, apresentar dados, criaçaõ de objetos, tryCatch e usanod location.href

--- js/script.js
+++ js/script.js
@@ -1,22 +1,56 @@
-// Capturando o botão de entrar 
+
+//Objeto INTERNO
+const usuarioUm = {
+    email:"email@email.com",
+    senha:"12345",
+    nome: "José Antonio",
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
+}
+
+
+//Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-//Função de dois parâmetros no botão. Evento de tela e função de callBack ou função anônima
-botaoEntrar.addEventListener("mouseover", function (){
 
-    const inputEmail = document.getElementById("idEmail");
-    const inputSenha = document.getElementById("idSenha");
+//Atrelando ao botão uma função que vai receber dois parâmetros.
+// O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
-//Dados em objeto
-//Declarando
-const usuario = {
-    email : inputEmail.value,
-    senha : inputSenha.value
-};
+botaoEntrar.addEventListener("click", function(e){
+    
+    e.preventDefault();
 
-console.log(`usuário ${usuario}`)
+    //Para casa:
+    //Capturar os campos de email e senha.
+    //Armazenar em variáveis.
+    //Imprimir seus valores.
+    const inputEmail = document.querySelector("#idEmail");
+    const inputSenha = document.querySelector("#idSenha");
+    
+    //Armazenar os dados que chegam do form em um objeto:
+    const userForm = {
+        email: inputEmail.value,
+        senha: inputSenha.value
+    }
 
-    console.log(`Email ${inputEmail.value} e senha ${inputSenha.value}`);
+    //Validação
+    try {
+
+        if (usuarioUm) {
+
+            if( (usuarioUm.email === userForm.email) && (usuarioUm.senha === userForm.senha)){
+                alert("Login realizado com sucesso!");
+                //Redirecionamento do usuário para uma nova página!!
+                window.location.href = "../index.html";
+            }else{
+                throw Error("Email ou Senha incorretos.");
+            }
+            
+        }else{
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
+        }
+
+    } catch (error) {
+        console.error(error);
+        alert(error);
+    }
+
 });
-
-
-
