78eb44809f7bc2389e749b2aab5d7e54ce0b5045
lenahlawi07 <lynnhalawi07@gmail.com>
2026-05-14 09:32:44 -0300

    feat : Estruturando o modalDialog com CSS e JS, adcionando o botao de controle para fechar.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,22 @@
 
-//objeto INTERNO
+//Objeto INTERNO
 const usuarioDb = {
-    email: "email@email.com",
-    senha: "12345",
-    nome: "Jose Antonio",
-    avatar: "https://placehold.co/100x100/eceaea/969696.png?text=usuario&font=lato"
+    email:"email@email.com",
+    senha:"12345",
+    nome: "José Antonio",
+    avatar: "https://placehold.co/100x100/e6e6e6/444444.png?text=usu%C3%A1rio&font=roboto"
 }
 
 
 //Capturando o botão de entrar
 const botaoEntrar = document.getElementById("btnEntrar");
-console.log(botaoEntrar);
 
 //Atrelando ao botão uma função que vai receber dois parâmetros.
 // O primeiro será o evento de tela, o segundo será a função de callBack ou função anônima.
 
 botaoEntrar.addEventListener("click", function(e){
     
-    e.preventDefault()
-
+    e.preventDefault();
 
     //Para casa:
     //Capturar os campos de email e senha.
@@ -26,29 +24,44 @@ botaoEntrar.addEventListener("click", function(e){
     //Imprimir seus valores.
     const email = document.querySelector("#idEmail");
     const senha = document.querySelector("#idSenha");
-    console.log(email.value);
-    console.log(senha.value); 
-
-    //armazenar os dados que chegam do form em um objeto:
+    
+    //Armazenar os dados que chegam do form em um objeto:
     const userForm = {
         email: email.value,
         senha: senha.value
     }
 
-    //validacao
+    //Validação
     try {
+
         if (usuarioDb) {
-            
-            if((usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
-                alert("Login realizado com sucesso");
-                //redirecionamento do usuario para uma nova pagina!!
-                window.location.href = "../index.html"
-             }else{
+
+            if( (usuarioDb.email === userForm.email) && (usuarioDb.senha === userForm.senha)){
+                //Capturar o dialog
+                const modalDialog = document.querySelector("#meuModal");
+
+                //Abrindo o dialog e deixando o fundo livre.
+                // modalDialog.show();
+
+                //Abrindo o dialog e travando o fundo.
+                modalDialog.showModal();
+
+                //Capturar o botão do modal para atrelar nele o evento de click e assim colocar  neste botão a ação de fechar o modal.
+                 const botaoModal = document.querySelector("#btnFechar");
+                botaoModal.addEventListener("click", function(){
+                    modalDialog.close();
+                });
+
+                //Redirecionamento do usuário para uma nova página!!
+                //window.location.href = "../index.html";
+            }else{
                 throw Error("Email ou Senha incorretos.");
-             }
+            }
+            
         }else{
-            throw Error("Ocorreu um erro no acesso as informacoes do usuario.")
+            throw Error("Ocorreu um erro no acesso as informações do usuário.");
         }
+
     } catch (error) {
         console.error(error);
         alert(error);
